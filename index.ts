fba3d8cdb879ba4de29ea91cfbc4e8811f447568
miracletjf <1398988546@qq.com>
2020-09-01 00:54:32 +0800

    feat: 完成基本功能开发。 1. 可以请求任意存在文件。 2. 对不存在的文件进行 404 处理。

--- index.ts
+++ index.ts
@@ -1,11 +1,36 @@
 import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
+import * as url from "url";
+import * as path from "path";
+import * as fs from "fs";
 
 const server = http.createServer();
+const staticPath = path.resolve(__dirname, 'public')
 
 server.on('request',(request:IncomingMessage, response:ServerResponse) => {
-  response.write('request success!\n');
-  response.end();
+  const { pathname, query } = url.parse(request.url)
+  const filePath = path.resolve(staticPath, pathname.substr(1) || 'index.html')
+  const reqArr = []
+  request.on('data', chunk => {
+    reqArr.push(chunk)
+  })
+  request.on('end', () => {
+    const reqBody = Buffer.concat(reqArr).toString()
+    fs.readFile(filePath, (err, data) => {
+      if(err) return handleErr(err, response)
+      response.write(data)
+      response.end()
+    })
+
+  })
 })
 
 server.listen(8888,'localhost')
+
+const handleErr = (err, response) => {
+  if(err.errno === -4058) {
+    response.statusCode = 404;
+    response.write();
+  }
+  response.end();
+}
